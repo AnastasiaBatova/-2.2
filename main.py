d7102f5b0fd64f245fd1ff49d01d3b9f3950adf2
AnastasiaBatova <batosha9913@icloud.com>
2024-11-24 22:55:38 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+course_name = "Python"
+print(course_name )
+all_the_task=12
+print(all_the_task)
+spent_hours=1.5
+print(spent_hours)
+average_execution_time = spent_hours/all_the_task
+print(average_execution_time)
